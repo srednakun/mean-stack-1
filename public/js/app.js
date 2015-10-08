1db2b7de3c2c1f2f54bf67a741a3d0b17edf0b4c
James Norton <james.norton@me.com>
2015-10-07 22:58:41 -0700

    add angular to make blog responsive, hard code test data in app.js file to be replaced with calls to mongo

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,43 @@
 (function() {
   var app = angular.module('app', []);
 
+  app.controller('BlogController', function() {
+    this.blogs = blogs;
+
+  });
+
+  var blogs = [
+
+  {
+    title: 'Integer dictum erat et',
+    author: 'Goose',
+    content: 'The secular cooling that must someday overtake our planet has already gone far indeed with our neighbour. Its physical condition is still largely a mystery, but we know now that even in its equatorial region the midday temperature barely approaches that of our coldest winter. Its air is much more attenuated than ours its oceans have shrunk until they ',
+    date: 'August 23, 1976'
+
+  },
+
+  {
+    title: 'Integer dictum erat et',
+    author: 'Maverick',
+    content: 'The secular cooling that must someday overtake our planet has already gone far indeed with our neighbour. Its physical condition is still largely a mystery, but we know now that even in its equatorial region the midday temperature barely approaches that of our coldest winter. Its air is much more attenuated than ours its oceans have shrunk until they ',
+    date: 'August 23, 1976'
+
+  },
+    {
+    title: 'Integer dictum erat et',
+    author: 'Viper',
+    content: 'The secular cooling that must someday overtake our planet has already gone far indeed with our neighbour. Its physical condition is still largely a mystery, but we know now that even in its equatorial region the midday temperature barely approaches that of our coldest winter. Its air is much more attenuated than ours its oceans have shrunk until they ',
+    date: 'August 23, 1976'
+
+  },
+    {
+    title: 'Integer dictum erat et',
+    author: 'Iceman',
+    content: 'The secular cooling that must someday overtake our planet has already gone far indeed with our neighbour. Its physical condition is still largely a mystery, but we know now that even in its equatorial region the midday temperature barely approaches that of our coldest winter. Its air is much more attenuated than ours its oceans have shrunk until they ',
+    date: 'August 23, 1976'
+
+  }
+
+  ];
 
 })();
