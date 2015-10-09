715cc2c040de12b20b143a15aa7fccf55d3d9f5c
Peter Strumolo <Peter Strumolo>
2015-10-08 18:02:02 -0700

    Add a form partial template

--- public/js/app.js
+++ public/js/app.js
@@ -10,26 +10,26 @@
   // $routeProvider is a provider in the ngRoute module
   // used for configuring routes.
   // App routes are declared with $routeProvider.
-  app.config("$routeProvider", function($routeProvider) {
+  app.config(function ($routeProvider) {
       // The $location service parses the URL in the browser address bar (based on the window.location) and makes the URL available to your application
       // This method is getter / setter.
 
       // Return path of current url
-      $routeProvider.
+      $routeProvider
       // Adds a new route definition to the $route service.
       // The route path (/blog-post) is (matched against $location.path)
       // which is a getter/setter that parses the address bar url
       // and makes it available to our app
-      when('/blog-post', {
+      .when('/blog-post', {
         // This is the route object:
         templateUrl: 'js/partials/blog-post.html',
         controller: "BlogCtrl as vm"
-      }).
-      when('/blog-post/:blog_id', {
-        templateUrl: "js/partials/blog-post-detail.html",
-        controller: "BlogsCtrl as vm"
-      }).
-      otherwise({
+      })
+      // when('/blog-post/:blog_id', {
+      //   templateUrl: "js/partials/blog-post-detail.html",
+      //   controller: "BlogsCtrl as vm"
+      // }).
+      .otherwise({
         redirectTo: "/blog-post"
       });
     });
--- public/js/blog.ctrl.js
+++ public/js/blog.ctrl.js
@@ -1,17 +1,39 @@
 (function () {
-  angular.module("app").controller("BlogCtrl", ["BlogsService", "$routeParams", function (BlogsService, $routeParams) {
+  angular.module("app").controller("BlogCtrl", ["$http", function ($http) {
     var vm = this;
+    vm.posts = [];
     var urlRoot = "/api/blog-post";
 
     initialize();
 
-    function initialize() {
-      BlogsService
-        .get($routeParams.blog_id)
-        .then(function (resp) {
-          vm.blog = resp.data;
-        });
+    // function initialize() {
+    //   BlogsService
+    //     .get($routeParams.blog_id)
+    //     .then(function (resp) {
+    //       vm.blog = resp.data;
+    //     });
+    // }
+
+    function initialize () {
+      getPosts();
     }
+
+    function get (id) {
+      if (angular.isDefined(id)) {
+          return $http.get(urlRoot + "/" + id);
+        } else {
+          return $http.get(urlRoot);
+        }
+    }
+
+    function getPosts () {
+      get().then(function (resp) {
+        vm.posts = resp.data;
+      });
+    }
+
+    console.log(vm.posts);
+
   }]);
 })();
 
