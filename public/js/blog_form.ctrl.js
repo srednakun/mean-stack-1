7fa01171eff222f70ebedc7df4dc13db3f0c2022
Peter Strumolo <Peter Strumolo>
2015-10-09 11:12:37 -0700

    Update to form controller

--- public/js/blog_form.ctrl.js
+++ public/js/blog_form.ctrl.js
@@ -6,32 +6,51 @@
 
     var vm = this;
 
+    var urlRoot = "/api/blog-post";
+
     vm.save = submitForm;
 
-    vm.post = {};
+    vm.blog = {};
 
-    initialize();
 
-    function initialize () {
-      if ($routeParams.blog_id) {
-        BlogsService.get($routeParams.blog_id).then(function (resp) {
-          vm.post = resp.data;
-        });
-      }
+
+    // initialize();
+
+    // function get (id) {
+    //   if (angular.isDefined(id)) {
+    //     return $http.get(urlRoot + "/" + id);
+    //   } else {
+    //     return $http.get(urlRoot);
+    //   }
+    // }
+
+    // function initialize () {
+    //   if ($routeParams.blog_id) {
+    //     get($routeParams.blog_id).then(function (resp) {
+    //       vm.blog = resp.data;
+    //     });
+    //   }
+    // }
+
+    function create(model) {
+        return $http.post(urlRoot, model);
     }
 
     function submitForm () {
 
       var method;
 
-      if (angular.isDefined(vm.post.number)) {
-        vm.post.number = parseInt(vm.post.number, 10);
-      }
+      // if (angular.isDefined(vm.post.number)) {
+      //   vm.post.number = parseInt(vm.post.number, 10);
+      // }
 
-      method = $routeParams.blog_id ? "update" : "create";
-      BlogsService.create(vm.post).then(function (resp) {
-        $location.path("/blog-post/" + resp.data._id);
+      // method = $routeParams.blog_id ? "update" : "create";
+      create(vm.blog).then(function (resp) {
+        console.log(resp.data + "heloooooo");
+// $location.path("/blog-post/" + resp.data._id);
       });
     }
   }]);
 }());
+
+
